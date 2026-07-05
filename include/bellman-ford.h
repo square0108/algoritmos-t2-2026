c24da5801f1dc973bf626542e3bebe3bb3a87ab0
square0108 <square@heckin.how>
2026-07-04 22:41:26 -0400

    fix: bugs from no constructor

--- include/bellman-ford.h
+++ include/bellman-ford.h
@@ -11,25 +11,39 @@ struct BellmanFordResult
   std::vector<size_t> predecessors;
   size_t root;
 
-  void print_distances(bool zero_indexed=true) {
+  BellmanFordResult(){}
+
+  BellmanFordResult(bool nc, std::vector<T> &d, std::vector<size_t> &p, size_t r)
+  {
+    no_negative_cycles = nc;
+    distances = d;
+    predecessors = p;
+    root = r;
+  }
+
+  void print_distances(bool zero_indexed = true)
+  {
     const T INFTY = std::numeric_limits<T>::max();
     auto print_idx = zero_indexed ? 0 : 1;
-    
-    for (size_t i = 0; i < distances.size(); i++) {
+
+    for (size_t i = 0; i < distances.size(); i++)
+    {
       if (distances[i] == INFTY)
         std::cout << "INF";
-      else 
+      else
         std::cout << distances[i];
       std::cout << "\t";
     }
     std::cout << std::endl;
   }
 
-  void print_predecessors(bool zero_indexed=true) {
+  void print_predecessors(bool zero_indexed = true)
+  {
     const T INFTY = std::numeric_limits<T>::max();
     auto print_idx = zero_indexed ? 0 : 1;
 
-    for (size_t i = 0; i < predecessors.size(); i++) {
+    for (size_t i = 0; i < predecessors.size(); i++)
+    {
       if (predecessors[i] == NO_PREDECESSOR)
         std::cout << "NIL";
       else
@@ -49,7 +63,8 @@ Retorna una 3-tupla con tres objetos:
 - `std::vector<size_t>`: Lista donde el elemento `i`-ésimo es el predecesor del vértice `i`-ésimo en un camino más corto desde `root` a `i`.
 */
 template <typename T>
-BellmanFordResult<T> Bellman_Ford(AdjacencyList<T>& g_adj_list, size_t root) {
+BellmanFordResult<T> Bellman_Ford(AdjacencyList<T> &g_adj_list, size_t root)
+{
   std::vector<T> dist_to_root;
   std::vector<size_t> predecessor;
   const T INFTY = std::numeric_limits<T>::max();
@@ -61,16 +76,20 @@ BellmanFordResult<T> Bellman_Ford(AdjacencyList<T>& g_adj_list, size_t root) {
   dist_to_root[root] = T();
 
   // Loop principal
-  for (size_t i = 0; i < n_verts-1; i++) {
-    
+  for (size_t i = 0; i < n_verts - 1; i++)
+  {
+
     // relajar todas las aristas que salen desde el vértice u
-    for (size_t u = 0; u < n_verts; u++) {
+    for (size_t u = 0; u < n_verts; u++)
+    {
       // para evitar overflow, y además no se actualizará nada si dist(u) es infinita
       if (dist_to_root[u] == INFTY)
         continue;
 
-      for (typename AdjacencyList<T>::EdgeHead edge : g_adj_list[u]){ 
-        if (dist_to_root[edge.head_vertex] > dist_to_root[u] + edge.weight) {
+      for (typename AdjacencyList<T>::EdgeHead edge : g_adj_list[u])
+      {
+        if (dist_to_root[edge.head_vertex] > dist_to_root[u] + edge.weight)
+        {
           dist_to_root[edge.head_vertex] = dist_to_root[u] + edge.weight;
           predecessor[edge.head_vertex] = u;
         }
@@ -80,29 +99,34 @@ BellmanFordResult<T> Bellman_Ford(AdjacencyList<T>& g_adj_list, size_t root) {
   }
 
   // Check de ciclos negativos con un último barrido de las aristas
-  for (size_t u = 0; u < n_verts; u++) {
-    for (typename AdjacencyList<T>::EdgeHead edge : g_adj_list[u]) {
+  for (size_t u = 0; u < n_verts; u++)
+  {
+    for (typename AdjacencyList<T>::EdgeHead edge : g_adj_list[u])
+    {
       if (dist_to_root[u] == INFTY) // u es inalcanzable desde root, no es posible detectar ciclo
         continue;
-      
-      if (dist_to_root[edge.head_vertex] > dist_to_root[u] + edge.weight) {
-        BellmanFordResult<T> result{ false, dist_to_root, predecessor, root };
+
+      if (dist_to_root[edge.head_vertex] > dist_to_root[u] + edge.weight)
+      {
+        BellmanFordResult<T> result{false, dist_to_root, predecessor, root};
         return result;
       }
     }
   }
 
-  BellmanFordResult<T> result{ true, dist_to_root, predecessor, root };
+  BellmanFordResult<T> result{true, dist_to_root, predecessor, root};
   return result;
 }
 
 // ¿Medir tiempo de creación de lista de adyacencia en experimentos? (pasar grafo "raw" a Bellman-Ford APSP)
 // ¿o pasarle la lista de adyacencia ya hecha?
 template <typename T>
-std::vector<BellmanFordResult<T>> Bellman_Ford_APSP(AdjacencyList<T> adjlist) {
+std::vector<BellmanFordResult<T>> Bellman_Ford_APSP(AdjacencyList<T> adjlist)
+{
   size_t n_verts = adjlist.size();
   std::vector<BellmanFordResult<T>> apsp_result(n_verts);
-  for (size_t v = 0; v < n_verts; v++) {
+  for (size_t v = 0; v < n_verts; v++)
+  {
     apsp_result[v] = Bellman_Ford(adjlist, v);
   }
   return apsp_result;
@@ -110,7 +134,8 @@ std::vector<BellmanFordResult<T>> Bellman_Ford_APSP(AdjacencyList<T> adjlist) {
 
 /* Overload que acepta matriz de adyacencia y construye lista de adyacencia antes de llamar BF. */
 template <typename T>
-std::vector<BellmanFordResult<T>> Bellman_Ford_APSP(Graph<T> adjmat) {
+std::vector<BellmanFordResult<T>> Bellman_Ford_APSP(Graph<T> adjmat)
+{
   AdjacencyList<T> adjlist = AdjacencyList<T>(adjmat);
   return Bellman_Ford_APSP(adjlist);
 }
--- include/floyd-warshall.h
+++ include/floyd-warshall.h
@@ -4,15 +4,28 @@
 #include "graph.h"
 
 template <typename T>
-struct FloydWarshallResult {
+struct FloydWarshallResult
+{
   bool no_negative_cycles;
-  std::vector<std::vector<T> > distances;
-  std::vector<std::vector<size_t> > predecessors;
+  std::vector<std::vector<T>> distances;
+  std::vector<std::vector<size_t>> predecessors;
   T INFTY = std::numeric_limits<T>::max();
 
-  void print_distances(bool zero_indexed = true) {
-    for (size_t i = 0; i < distances.size(); i++) {
-      for (size_t j = 0; j < distances.size(); j++) {
+  FloydWarshallResult(){}
+
+  FloydWarshallResult(bool nc, std::vector<std::vector<T>> &d, std::vector<std::vector<size_t>> &p)
+  {
+    no_negative_cycles = nc;
+    distances = d;
+    predecessors = p;
+  }
+
+  void print_distances(bool zero_indexed = true)
+  {
+    for (size_t i = 0; i < distances.size(); i++)
+    {
+      for (size_t j = 0; j < distances.size(); j++)
+      {
         if (distances[i][j] == INFTY)
           std::cout << "INF\t";
         else
@@ -22,9 +35,12 @@ struct FloydWarshallResult {
     }
   }
 
-  void print_predecessors(bool zero_indexed = true) {
-    for (size_t i = 0; i < predecessors.size(); i++) {
-      for (size_t j = 0; j < predecessors.size(); j++) {
+  void print_predecessors(bool zero_indexed = true)
+  {
+    for (size_t i = 0; i < predecessors.size(); i++)
+    {
+      for (size_t j = 0; j < predecessors.size(); j++)
+      {
         if (predecessors[i][j] == NO_PREDECESSOR)
           std::cout << "NIL\t";
         else
@@ -36,14 +52,16 @@ struct FloydWarshallResult {
 };
 
 template <typename T>
-FloydWarshallResult<T> Floyd_Warshall(Graph<T>& graph) {
-  // Como en cualquier momento sólo se utilizan recursiones nivel (k-1) y (k), 
+FloydWarshallResult<T> Floyd_Warshall(Graph<T> &graph)
+{
+  // Como en cualquier momento sólo se utilizan recursiones nivel (k-1) y (k),
   // sólo se necesitan 2 matrices adicionales, en vez de guardar todas desde D^(0) hasta D^(n).
 
   size_t n_verts = graph.size();
-  std::vector<std::vector<T> > distances_k1, distances_k2;
-  std::vector<std::vector<size_t> > predecessors_k1, predecessors_k2;
+  std::vector<std::vector<T>> distances_k1, distances_k2;
+  std::vector<std::vector<size_t>> predecessors_k1, predecessors_k2;
   T INFTY = std::numeric_limits<T>::max();
+  bool no_negative_cycle_flag = true;
 
   distances_k1 = graph.adj_mat; // D^(0)
   distances_k2.resize(n_verts, std::vector<T>(n_verts, INFTY));
@@ -51,9 +69,11 @@ FloydWarshallResult<T> Floyd_Warshall(Graph<T>& graph) {
   predecessors_k2.resize(n_verts, std::vector<size_t>(n_verts, NO_PREDECESSOR));
 
   // Inicialización de \Pi^(0) (predecessors_k1)
-  for (size_t i = 0; i < n_verts; i++) {
+  for (size_t i = 0; i < n_verts; i++)
+  {
     predecessors_k1[i].resize(n_verts);
-    for (size_t j = 0; j < n_verts; j++) {
+    for (size_t j = 0; j < n_verts; j++)
+    {
       if (i == j || graph[i][j] == INFTY)
         predecessors_k1[i][j] = NO_PREDECESSOR;
       else
@@ -68,45 +88,49 @@ FloydWarshallResult<T> Floyd_Warshall(Graph<T>& graph) {
   std::vector<std::vector<size_t>> *k_pred;
   std::vector<std::vector<size_t>> *k_minus1_pred;
 
-  for (size_t k = 0; k < n_verts; k++) {
+  for (size_t k = 0; k < n_verts; k++)
+  {
     // Matriz (k) inicial es siempre k1, (k-1) inicial es k2.
-    if (k % 2 == 0) {
+    if (k % 2 == 0)
+    {
       k_minus1_dist = &distances_k1;
       k_minus1_pred = &predecessors_k1;
       k_dist = &distances_k2;
       k_pred = &predecessors_k2;
     }
-    else {
+    else
+    {
       k_minus1_dist = &distances_k2;
       k_minus1_pred = &predecessors_k2;
       k_dist = &distances_k1;
       k_pred = &predecessors_k1;
     }
 
-    for (size_t i = 0; i < n_verts; i++) {
-      for (size_t j = 0; j < n_verts; j++) {
+    for (size_t i = 0; i < n_verts; i++)
+    {
+      for (size_t j = 0; j < n_verts; j++)
+      {
         // Primeras 2 condiciones para evitar overflow
         // Si la distancia k-1 no mejora con el detour, se conserva dist(k-1) en dist(k).
-        if ((*k_minus1_dist)[i][k] == INFTY 
-        || (*k_minus1_dist)[k][j] == INFTY
-        || (*k_minus1_dist)[i][j] <= (*k_minus1_dist)[i][k] + (*k_minus1_dist)[k][j]) 
+        if ((*k_minus1_dist)[i][k] == INFTY || (*k_minus1_dist)[k][j] == INFTY || (*k_minus1_dist)[i][j] <= (*k_minus1_dist)[i][k] + (*k_minus1_dist)[k][j])
         {
           (*k_dist)[i][j] = (*k_minus1_dist)[i][j];
           (*k_pred)[i][j] = (*k_minus1_pred)[i][j];
         }
 
-        else /*(distances_k[i][j] > distances_(k-1)[i][k] + distances_(k-1)[k][j])*/ {
+        else /*(distances_k[i][j] > distances_(k-1)[i][k] + distances_(k-1)[k][j])*/
+        {
           (*k_dist)[i][j] = (*k_minus1_dist)[i][k] + (*k_minus1_dist)[k][j];
-          (*k_pred)[i][j] = (*k_minus1_pred)[k][j]; 
+          (*k_pred)[i][j] = (*k_minus1_pred)[k][j];
         }
       }
       // Detección de ciclos negativos
       if ((*k_dist)[i][i] < T())
-        return FloydWarshallResult<T>{false, *k_dist, *k_pred};
+        no_negative_cycle_flag = false;
     }
   }
 
-  return FloydWarshallResult<T>{true, *k_dist, *k_pred};
+  return FloydWarshallResult<T>{no_negative_cycle_flag, *k_dist, *k_pred};
 }
 
 #endif
